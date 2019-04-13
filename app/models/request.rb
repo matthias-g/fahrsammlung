c778af9e48a454a3b4e0f7dd27d5927970d367d0
Matthias Grundmann <matthias@glasmail.de>
2019-04-13 10:33:52 +0200

    Add sorting option

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   include Item
 
-  validates_presence_of :title, :description, :start_location
+  validates_presence_of :title, :start_location
   validates_length_of :title, maximum: 140
 
 end
