605115d6eab1b04559a7d5fa677bf33b400a1d34
Matthias Grundmann <matthias@glasmail.de>
2019-12-17 23:49:20 +0100

    Sanitize map data

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -11,4 +11,12 @@ module WelcomeHelper
 	def devise_mapping
 		@devise_mapping ||= Devise.mappings[:user]
 	end
+
+	def welcome_sanitize(string)
+		string.gsub("\n", " ").gsub("\r", "").to_json[1...-1]
+	end
+
+	def welcome_sanitize_int(integer)
+		integer.to_json[1...-1]
+	end
 end
