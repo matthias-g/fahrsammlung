89ee98c9da1f304a2d166eb1bfdcdf5b18f82ddc
Matthias Grundmann <matthias@glasmail.de>
2020-01-21 23:03:45 +0100

    Add revive scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,47 @@
 Rails.application.routes.draw do
+  scope 'revive' do
+    get 'stats/counts' => 'stats#counts'
+    get 'stats/statistiken' => 'stats#statistics'
 
-  get 'stats/counts' => 'stats#counts'
-  get 'stats/statistiken' => 'stats#statistics'
+    post 'subscriptions/create'
+    delete 'subscriptions/destroy'
+    get 'subscription/confirm/:confirmation_token' => 'subscriptions#confirm', as: 'subscriptions_confirm'
+    get 'subscription/unsubscribe/:item_type/:unsubscribe_token' => 'subscriptions#destroy', as: 'subscriptions_unsubscribe'
+    delete 'subscription/unsubscribe' => 'subscriptions#unsubscribe_user', as: 'subscriptions_unsubscribe_user'
 
-  post 'subscriptions/create'
-  delete 'subscriptions/destroy'
-  get 'subscription/confirm/:confirmation_token' => 'subscriptions#confirm', as: 'subscriptions_confirm'
-  get 'subscription/unsubscribe/:item_type/:unsubscribe_token' => 'subscriptions#destroy', as: 'subscriptions_unsubscribe'
-  delete 'subscription/unsubscribe' => 'subscriptions#unsubscribe_user', as: 'subscriptions_unsubscribe_user'
+    get 'users/items'
 
-  get 'users/items'
+    get 'reactivate/:token' => 'item_reactivation#reactivate', as: 'reactivate'
 
-  get 'reactivate/:token' => 'item_reactivation#reactivate', as: 'reactivate'
+    get 'welcome/index'
+    ActiveAdmin.routes(self)
 
-  get 'welcome/index'
-  ActiveAdmin.routes(self)
+    devise_for :users, :controllers => {confirmations: 'confirmations'}
 
-  devise_for :users, :controllers => {confirmations: 'confirmations'}
-
-  resources :offers do
-    member do
-      put :toggle_active
-      get :owner_show
-      post :request_owner_link
+    resources :offers do
+      member do
+        put :toggle_active
+        get :owner_show
+        post :request_owner_link
+      end
     end
-  end
 
-  resources :requests do
-    member do
-      put :toggle_active
-      get :owner_show
-      post :request_owner_link
+    resources :requests do
+      member do
+        put :toggle_active
+        get :owner_show
+        post :request_owner_link
+      end
     end
+
+    resources :answers, only: [:create]
+
+    # Error handling
+    get '/404' => redirect('/errors/404')
+    get '/422' => redirect('/errors/422')
+    get '/500' => redirect('/errors/500')
+    get '/errors/:error_code', to: 'errors#error'
+
+    root to: 'welcome#index'
   end
-
-  resources :answers, only: [:create]
-
-  # Error handling
-  get '/404' => redirect('/errors/404')
-  get '/422' => redirect('/errors/422')
-  get '/500' => redirect('/errors/500')
-  get '/errors/:error_code', to: 'errors#error'
-
-  root to: 'welcome#index'
 end
